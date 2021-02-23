b4cfca68e5d73e4dd02de03a2b13bfb1737f5d95
Katie <katieberrym@gmail.com>
2021-02-23 13:36:39 -0800

    created app test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,16 @@ describe('shorten-be routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('creates a short url via post', async () => {
+    return request(app)
+      .post('/api/v1/shorten')
+      .send({ url: 'http://test.com/a/very/long/url' })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.stringMatching(/^\w{8}$/),
+          originalUrl: 'http://test.com/a/very/long/url'
+        });
+      });
+  });
 });
